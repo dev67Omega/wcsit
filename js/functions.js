d0a6f1c3e0d41c054af5316306ff02e6518cd866
Mikhail-Bonjour <mike.prodesign86@gmail.com>
2022-03-18 11:36:00 -0600

    viernes 11:35

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,21 @@
-var app = angular.module("myApp", ['ngRoute','ui.bootstrap']);
+
 
 function closeMenu() {
     var element = document.getElementById("navbarSupportedContent");
     element.classList.remove("show");
-  }
+}
 
-  window.onscroll = function() {
-    
-    scrollFunction()};
-  
-  function scrollFunction() {
 
-    var navPrincipal = document.getElementById("navBarra");
-    if (document.body.scrollTop > 768 || document.documentElement.scrollTop > 768) {
-      navPrincipal.classList.remove("navDark");
-    } else {
-      navPrincipal.classList.add("navDark");
-    }
+function scrollFunction() {
+
+  var navPrincipal = document.getElementById("navBarra");
+  if (document.body.scrollTop > 768 || document.documentElement.scrollTop > 768) {
+    navPrincipal.classList.remove("navDark");
+  } else {
+    navPrincipal.classList.add("navDark");
   }
+}
+
+window.onscroll = function() {
+    scrollFunction()
+  };
