61fe4782d39417753ea5ce79e8fbefb4b12baee8
Mikhail Bonjour <35705469+Mikhail-Bonjour@users.noreply.github.com>
2022-03-19 17:14:01 -0600

    Final version

--- js/functions.js
+++ js/functions.js
@@ -1,55 +1,39 @@
 function closeMenu() {
-    var element = document.getElementById("navbarSupportedContent");
+    var element = document.getElementById("collapsibleNavbar");
     element.classList.remove("show");
 }
 
 
 window.onscroll = function() {
-  scrollNav();
-  scrollFunction();
+    scrollNav();
+    scrollFunction();
 };
 
+var carH = document.getElementById("imgAlto").height;
+let carI = 100;
+var h = carH - carI;
 
 function scrollNav() {
-  var carH = document.getElementById("imgAlto").height;
-  var h = carH;
-  var navPrincipal = document.getElementById("navBarra");
-  if (document.body.scrollTop > h || document.documentElement.scrollTop > h) {
-    navPrincipal.classList.remove("navDark");
-  } else {
-    navPrincipal.classList.add("navDark");
-  }
+    var navPrincipal = document.getElementById("navBarra");
+    if (document.body.scrollTop > h || document.documentElement.scrollTop > h) {
+        navPrincipal.classList.remove("navDark");
+    } else {
+        navPrincipal.classList.add("navDark");
+    }
 }
 
- 
- function scrollFunction() {
-  var carH = document.getElementById("imgAlto").height;
-  var h = carH;
-    var topbutton = document.getElementById("topBtn");
 
+function scrollFunction() {
+    var topbutton = document.getElementById("topBtn");
     if (document.body.scrollTop > h || document.documentElement.scrollTop > h) {
-      topbutton.style.display = "block";
+        topbutton.style.display = "block";
     } else {
-      topbutton.style.display = "none";
+        topbutton.style.display = "none";
     }
-  }
-  
-  // Cuando se de click en el boton nos lleva al tope
-  function topFunction() {
+}
+
+// Cuando se de click en el boton nos lleva al tope
+function topFunction() {
     document.body.scrollTop = 10; // For Safari
     document.documentElement.scrollTop = 10; // Para Chrome, Firefox, IE y Opera
-  } 
-  
-
-// var app = angular.module('todoApp', ['ngRoute']);
-
-// app.config(function($routeProvider){
-// 	$routeProvider
-// 	.when('/', {
-// 		// templateUrl: 'sites/login.html',
-// 		controller: 'mailCtrl'
-// 	})
-// 	.otherwise({
-// 		redirectTo: '/'
-// 	});
-// });
+}
