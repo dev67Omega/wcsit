fef5b923575ae9f042335537e412412d119ee0bb
Mikhail-Bonjour <mike.prodesign86@gmail.com>
2022-03-18 18:08:57 -0600

    viernes 1800

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,15 @@
-
 function closeMenu() {
     var element = document.getElementById("navbarSupportedContent");
     element.classList.remove("show");
 }
 
 
-function scrollFunction() {
+window.onscroll = function() {
+  scrollNav();
+  scrollFunction();
+};
+
+function scrollNav() {
 
   var navPrincipal = document.getElementById("navBarra");
   if (document.body.scrollTop > 768 || document.documentElement.scrollTop > 768) {
@@ -15,11 +19,23 @@ function scrollFunction() {
   }
 }
 
-window.onscroll = function() {
-    scrollFunction()
-  };
+  
+ function scrollFunction() {
+    var topbutton = document.getElementById("topBtn");
 
-
+    if (document.body.scrollTop > 768 || document.documentElement.scrollTop > 768) {
+      topbutton.style.display = "block";
+    } else {
+      topbutton.style.display = "none";
+    }
+  }
+  
+  // Cuando se de click en el boton nos lleva al tope
+  function topFunction() {
+    document.body.scrollTop = 10; // For Safari
+    document.documentElement.scrollTop = 10; // Para Chrome, Firefox, IE y Opera
+  } 
+  
 
 // var app = angular.module('todoApp', ['ngRoute']);
 
